6d9d84af8c2bc2569853c0ac006a12d7740d0744
rahul8674 <rahulchhabra9000@gmail.com>
2025-02-02 14:20:31 +0530

    updated firebaseConfig

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrfW8NwODeLqu8Xq1Yca1zTmmHu-7vVuA",
-    authDomain: "react-project-126f2.firebaseapp.com",
-    projectId: "react-project-126f2",
-    storageBucket: "react-project-126f2.firebasestorage.app",
-    messagingSenderId: "224534177824",
-    appId: "1:224534177824:web:97d8641ba67c95aa5ef445"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
